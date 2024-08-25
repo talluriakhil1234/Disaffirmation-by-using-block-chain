adff6a678b5d583a8d2ab747dfff1dea05b5539f
akhil <talluriakhil1234@gmail.com>
2024-08-25 09:13:39 +0530

    initial commit

--- src/newproject_frontend/src/App.jsx
+++ src/newproject_frontend/src/App.jsx
@@ -1,31 +1,14 @@
 import { useState } from 'react';
 import { newproject_backend } from 'declarations/newproject_backend';
+import DailyAffirmation from './DailyAffirmation';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    newproject_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
-  return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+  return(
+    <>
+    <Daily Affirmations App />
+    </>
   );
+   
 }
 
 export default App;
